1717c9a0458aa4dc0bf156ec51636bf8b3e6bfac
Seb J <51689500+OfficialJavaScript@users.noreply.github.com>
2023-09-18 20:04:27 +1200

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,23 @@
+function home() {
+    window.location.href="index.html";
+}
+
+function forum() {
+    window.location.href="forum.html";
+}
+
+function members() {
+    window.location.href="members.html";
+}
+
+function about() {
+    window.location.href="about.html";
+}
+
+function login() {
+    window.location.href="login.html";
+}
+
+function signup() {
+    window.location.href="signup.html";
+}
